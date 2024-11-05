550f0544b05a49df7e48d5c9fd722aa3e57f575b
Eisyger <140314733+Eisyger@users.noreply.github.com>
2024-11-05 09:57:33 +0100

    impl auth

--- Program.cs
+++ Program.cs
@@ -2,10 +2,26 @@ using BergNotenWASM;
 using BergNotenWASM.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Headers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+#region Auth
+builder.Configuration.AddJsonFile("appsettings.json");
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    var azureOptions = builder.Configuration.GetSection("Azure");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add($"{azureOptions["ApiId"]}/.default");
+    options.ProviderOptions.Authority = $"https://login.microsoftonline.com/{azureOptions["TenantId"]}/";
+    options.ProviderOptions.ClientId = azureOptions["ClientId"];
+});
+#endregion 
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
